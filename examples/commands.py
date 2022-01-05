d93c4f4b44d3bf794825f0309748bafb8c2cfe27
TheCodersYT <96958404+TheCodersYT@users.noreply.github.com>
2022-01-05 02:08:25 +0000

    Update commands.py

--- examples/commands.py
+++ examples/commands.py
@@ -1,1 +1,10 @@
+import nextcord
+from nextcord.ext import commands
 
+bot = commands.Bot(command_prefix="?")
+
+@bot.command()
+async def reply(ctx, msg):
+    await ctx.send(msg + f" -{ctx.author}")
+    
+bot.run("token")
